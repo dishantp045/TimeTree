723daf3186910d11d4ee76e36f37e47b5aba7bd2
Dishant  patel <dishantpatel@10-94-bb-e6-23-20.wireless.temple.edu>
2019-06-13 15:41:53 -0400

    excluding url problems

--- src/screens/TimelineScreen.js
+++ src/screens/TimelineScreen.js
@@ -1,2 +1,25 @@
-import React, {Component} from 'react-native';
-import {Image, Animated, TouchableHighlight, Easing} from 'react-native';
+import React, { Component } from "react-native";
+import {
+  View,
+  Image,
+  Animated,
+  TouchableHighlight,
+  Easing,
+  StyleSheet
+} from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "dimgrey"
+  }
+});
+
+class TimelineScreen extends Components<Props> {
+    render() {
+        return ( 
+            <View styles = {styles.container}>
+            </View>
+        );
+    }
+}
